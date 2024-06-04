4b0df2ad9f7a52c32d9be8e9260fb5311496f437
Nemesis <sauulventura690@gmail.com>
2024-06-04 12:57:32 -0600

    t3

--- baquen/main.js
+++ baquen/main.js
@@ -11,7 +11,7 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
+    next(); 
   });
 app.set("new site","polar");
 app.use(express.urlencoded({
