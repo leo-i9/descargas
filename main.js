8859db95b2dd58ca54f9aa57c107b60cc3771f37
Nemesis <sauulventura690@gmail.com>
2024-07-26 21:38:58 -0600

    1.100

--- main.js
+++ main.js
@@ -2,9 +2,13 @@ import express,{ Router}from 'express';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import path from "path"
+import play from 'play-dl';
+import fs from 'fs';
+
+import ytdl from '@distube/ytdl-core';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-import { router } from './routerr.js';
+
 const app = express();
 
 app.use((req, res, next) => {
@@ -20,10 +24,11 @@ app.use(express.urlencoded({
 
 
 app.get('/main', (req, res) => {
+ 
   res.sendFile(path.join(__dirname, './interfas/index.html'));
   //res.sendFile('./interfas/index.html');
 });
-app.use("/polar",router)
+
 // app.get('/descargar', function(req, res) {
 //   const url = req.query.url; // La URL del video de YouTube viene de la consulta del usuario
 //   const video = youtubedl(url,
@@ -33,7 +38,40 @@ app.use("/polar",router)
 //   res.setHeader('Content-Disposition', 'attachment; filename="video.mp4"');
 //   video.pipe(res);
 // });
-  const port = process.env.PORT ?? 3000;
+
+app.get('/download', async (req, res) => {
+  const videoUrl = req.query.videoUrl;
+
+  if (!videoUrl) {
+    return res.status(400).send('URL del video es requerida');
+  }
+
+  try {
+    const videoStream = ytdl(videoUrl);
+
+    res.setHeader('Content-Disposition', 'attachment; filename="video.mp4"');
+    res.setHeader('Content-Type', 'video/mp4');
+
+    req.on('close', () => {
+      console.log('Solicitud abortada por el cliente');
+      videoStream.destroy();
+    });
+
+    videoStream.pipe(res)
+      .on('finish', () => {
+        console.log('Descarga completada');
+      })
+      .on('error', (err) => {
+        console.error('Error durante la descarga:', err);
+        res.status(500).send('Error durante la descarga');
+      });
+  } catch (err) {
+    console.error('Error:', err);
+    res.status(500).send('Error al procesar la solicitud');
+  }
+});
+
+  const port = process.env.PORT ?? 4000;
 app.listen(port, function() {
   console.log('Aplicación escuchando en el puerto 3000!');
 });
